779a17e040ea4fefe8b52fb03df7a8a961c50153
mannycov <manny.cov09@gmail.com>
2017-08-29 21:07:18 -0700

    added exampleVideoData src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
             <VideoPlayer video={window.exampleVideoData[0]}/>
           </div>
           <div className="col-md-5">
-            <VideoList videos={window.exampleVideoData} />
+            <VideoList videos={window.exampleVideoData}/>
           </div>
         </div>
       </div>
