ea2945f0ff486041f9984fc0953efa4cc3692114
mannycov <manny.cov09@gmail.com>
2017-09-01 13:16:47 -0700

    added searchYouTube helper function which successfully sends a GET request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    part: 'snippet',
+    videoEmbeddable: 'true',
+    type: 'video'
+  })
+    .done((items) => {
+      callback(window.exampleVideoData, items);
+    })
+    .fail((items) => {
+      console.log(failed);
+    });
 };
+
 
 window.searchYouTube = searchYouTube;
